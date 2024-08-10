bf1522751c222f7ec99f1dd41fac198044d3d467
sonyokpara <sony.okpara@yahoo.com>
2024-08-10 09:49:46 +0000

    Implement Update quantity feature

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -35,10 +35,15 @@ cart.forEach((cartItem) => {
                     <span> Quantity: <span class="quantity-label">${
                       cartItem.quantity
                     }</span> </span>
-                    <span class="update-quantity-link link-primary">
+                    <span class="js-update-quantity-link update-quantity-link link-primary" data-product-id="${
+                      matchingProduct.id
+                    }">
                     Update
                     </span>
-
+                    <input type="text" class="quantity-input js-quantity-input" />
+                    <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${
+                      matchingProduct.id
+                    }">Save</span>
                     <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${
                       matchingProduct.id
                     }>
@@ -100,6 +105,16 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   });
 });
 
+document.querySelectorAll(".js-update-quantity-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const { productId } = link.dataset;
+    const cartItemContainer = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    cartItemContainer.classList.add("is-editing-quantity");
+  });
+});
+
 function updateCartQuantity(calbck) {
-  document.querySelector(".js-checkout-quantity").innerHTML = calbck();
+  document.querySelector(".js-checkout-quantity-").innerHTML = calbck();
 }
