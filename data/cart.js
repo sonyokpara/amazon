decf8697af5f2a9e2507f4cf8218347677ff2fe6
sonyokpara <sony.okpara@yahoo.com>
2024-08-10 13:13:24 +0000

    save date and generate delivery options HTML

--- data/cart.js
+++ data/cart.js
@@ -5,14 +5,17 @@ if (!cart) {
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
+      deliveryOptionId: "1",
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
       quantity: 1,
+      deliveryOptionId: "2",
     },
     {
       productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
       quantity: 1,
+      deliveryOptionId: "3",
     },
   ];
 }
@@ -43,7 +46,7 @@ export function addToCart(productId) {
   if (matchingItem) {
     matchingItem.quantity += quantity;
   } else {
-    cart.push({ productId, quantity });
+    cart.push({ productId, quantity, deliveryOptionId: 1 });
   }
 
   saveToStorage();
